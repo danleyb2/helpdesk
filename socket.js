190a80efe85d61c053f37ae018d484e699cc7634
nyaundi brian <danleyb2@gmail.com>
2019-01-01 01:54:30 +0300

    dev updates

--- socket.js
+++ socket.js
@@ -9,6 +9,140 @@ var mongoose = require('mongoose');
 
 module.exports = function (io) {
 
+    var nsp = io.of('help');
+    nsp.on('connection', function (socket) {
+        console.log('someone connected');
+        socket.on('init chat', function (msg) {
+            async.waterfall([
+
+                    function (callback) {
+                        Contact.findOne({email: msg['email']}, function (err, contact) {
+                            if (contact == null) {
+                                Contact.create({
+                                    email: msg['email']
+
+                                }, function (err, contact) {
+                                    if (err) {
+                                        console.error(err);
+                                    }
+
+                                    return callback(null, contact);
+                                });
+                            } else {
+                                return callback(null, contact);
+                            }
+                        });
+                    },
+
+                    function (contact, callback) {
+
+                            if (msg['conversation']) {
+                                // find conversation
+                                Conversation.findOne({_id: msg['conversation']}, function (err, conversation) {
+                                    if (conversation == null) {
+                                        return callback(null, contact, null);
+
+                                    } else {
+                                        return callback(null, contact, conversation);
+                                    }
+                                });
+                            }else {
+                                return callback(null, contact, null);
+
+                            }
+
+
+                    },
+
+                    function (contact, conversation, callback) {
+                        if (conversation == null) {
+                            // create participant
+                            Participant.create({
+                                refModel: 'Contact',
+                                modelRef: contact._id
+                            }, function (err, participant) {
+                                if (err) {
+                                    console.error(err)
+                                }
+                                return callback(null, participant,conversation);
+                            });
+
+                        } else {
+                            let arr = conversation.participants.map(ele => new mongoose.Types.ObjectId(ele.id));
+
+                            Participant.findOne({modelRef: mongoose.Types.ObjectId(contact._id)})
+                                .where('_id')
+                                .in(arr)
+                                .exec(function (err, participant) {
+                                    if (err) {
+                                        console.error(err)
+                                    }
+
+
+                                    return callback(null, participant,conversation);
+
+                                });
+
+                        }
+
+                    },
+                    function (participant,conversation, callback) {
+                    if (conversation==null){
+                        Conversation.create({
+                            title: msg['text'],
+                            participants: [participant._id],
+                            property: '5c26f6140d1b524bdbc8adbd'
+                        }, function (err, conversation) {
+
+                            if (err) {
+                                console.error(err)
+                            }
+
+
+                            msg['conversation'] = conversation._id;
+                            return callback(null, conversation, participant);
+
+
+                        });
+
+                    } else {
+                        return callback(null, conversation, participant);
+
+                    }
+
+
+
+                    }
+
+                ], function (error, conversation, participant) {
+                    if (error) {
+                        console.error(error)
+                    }
+                    var message = Message.create({
+                        body: msg['text'],
+                        conversation: conversation._id,
+                        owner: participant._id,
+                    }, function (err, message) {
+                        if (err) console.error(err);
+
+                        msg['_id'] = message._id;
+                        var room = 'c/'+conversation._id;
+                        socket.join(room);
+                        io.of('help').to(room).emit('chat message', msg);
+
+                        socket.to(room).emit('chat message', msg);
+
+                    });
+                }
+            );
+
+        });
+
+
+    });
+    // nsp.emit('hi', 'everyone!');
+
+
     io.on("connection", function (socket) {
             console.log("new connection");
             var userId;
@@ -19,9 +153,11 @@ module.exports = function (io) {
                     socket.user = user;
                     // todo might be null
 
+
                 });
 
             }
+
 
             socket.on('new message', function (msg) {
                 console.log('new message: ' + msg);
@@ -34,6 +170,9 @@ module.exports = function (io) {
                     async.waterfall([
                         function (callback) {
                             conversation = Conversation.findOne({_id: msg['conversation']}, function (err, conversation) {
+
+
+
                                 callback(null, conversation);
                             });
                         },
@@ -42,31 +181,31 @@ module.exports = function (io) {
                             let arr = conversation.participants.map(ele => new mongoose.Types.ObjectId(ele.id));
 
                             // find participant
-                            Participant.findOne({ modelRef: mongoose.Types.ObjectId(socket.user._id)})
+                            Participant.findOne({modelRef: mongoose.Types.ObjectId(socket.user._id)})
                                 .where('_id')
                                 .in(arr)
                                 .exec(function (err, participant) {
 
-                                console.log(participant);
+                                    console.log(participant);
 
-                                // create participant
-                                if (participant==null) {
+                                    // create participant
+                                    if (participant == null) {
 
-                                    Participant.create({
-                                        refModel: 'Member',
-                                        modelRef: socket.user._id
-                                    }, function (err, newParticipant) {
-                                        return callback(null, conversation,newParticipant);
-                                    });
+                                        Participant.create({
+                                            refModel: 'Member',
+                                            modelRef: socket.user._id
+                                        }, function (err, newParticipant) {
+                                            return callback(null, conversation, newParticipant);
+                                        });
 
-                                }else {
-                                    return callback(null,conversation, participant);
-                                }
+                                    } else {
+                                        return callback(null, conversation, participant);
+                                    }
 
-                            });
+                                });
 
                         }
-                    ], function (error, conversation,participant) {
+                    ], function (error, conversation, participant) {
                         if (error) {
                             console.log(error);
                         }
@@ -82,6 +221,13 @@ module.exports = function (io) {
 
                             msg['_id'] = message._id;
                             io.emit('chat message', message);
+
+                            var room = 'c/'+conversation._id;
+                            // socket.join(room);
+                            io.of('help').to(room).emit('chat message', msg);
+
+
+
                         });
 
                     });
