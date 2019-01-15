73747bbf5823406c09f9ecb8fb8257cd2cce16c7
nyaundi brian <danleyb2@gmail.com>
2019-01-15 11:27:23 +0300

    dev updates - notifications

--- models/notification.js
+++ models/notification.js
@@ -1,13 +1,31 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 
+NotificationSchema = new Schema({
+    sender: {type:mongoose.Schema.Types.ObjectId, ref:'Account'}, // Notification creator
+    receivers: [
+        new mongoose.Schema({
+            account: {type:mongoose.Schema.Types.ObjectId, ref:'Account'},
+            read: {type: Boolean},
+            deleted: {type: Boolean,default:false}
+        }, {_id: false, timestamps: true})
+    ], // Receivers of the notification
+    action: String,
+    modelRef: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        // Instead of a hardcoded model name in `ref`, `refPath` means Mongoose
+        // will look at the `refModel` property to find the right model.
+        refPath: 'refModel'
+    },
+    refModel: {
+        type: String,
+        required: true,
+        enum: ['Ticket', 'Member']
+    }
+
+},{timestamps: true});
 
 
-var TypeSchema = mongoose.Schema({
-    property:   { type: mongoose.Schema.Types.ObjectId, ref: 'Property', required: true },
-    name:       { type: String, required: true, unique: false }
-
-}, {timestamps: true});
-
-
-module.exports = mongoose.model('Type', TypeSchema);
+module.exports = mongoose.model('Notification', NotificationSchema);
